573afe3daecfcc78666e18f7408845ca4ff40fe2
Ankur33657 <ankursingh943040@gmail.com>
2026-08-15 18:28:08 +0530

    fix/security

--- src/main/java/com/example/ecom_backend/services/Auth/AuthService.java
+++ src/main/java/com/example/ecom_backend/services/Auth/AuthService.java
@@ -1,7 +1,11 @@
 package com.example.ecom_backend.services.Auth;
 
+import com.example.ecom_backend.dto.User.AuthRequestDto;
 import com.example.ecom_backend.dto.User.CreateUserDto;
+import com.example.ecom_backend.dto.User.JwtResponseDto;
 
 public interface AuthService {
   String CreateUser(CreateUserDto user);
+  JwtResponseDto login(AuthRequestDto user);
+  String getAccessToken(String refreshToken);
 }
--- src/main/java/com/example/ecom_backend/services/Auth/JwtService.java
+++ src/main/java/com/example/ecom_backend/services/Auth/JwtService.java
@@ -1,44 +1,3 @@
-//package com.example.ecom_backend.services.Auth;
-//
-//
-//import io.jsonwebtoken.Jwts;
-//import io.jsonwebtoken.SignatureAlgorithm;
-//import io.jsonwebtoken.security.Keys;
-//import org.springframework.stereotype.Service;
-//
-//import java.util.Date;
-//
-//@Service
-//public class JwtService {
-//
-//    private static final String SECRET_KEY="Ankur336@#225TSR$$KarinSasukeNarutoLuffyZoroSanjiNami336@#$%**WorkingTogetherWithStrawHatCREW";
-//    private static final long ACCESSS_TOKEN_EXPIRATION= 15*60*1000;
-//    private static final long REFRESH_TOKEN_EXPIRATION= 1000*60*60*24;
-//
-//    public String generateToken(String username,boolean isAccessToken){
-//        long expiration=isAccessToken?ACCESSS_TOKEN_EXPIRATION:REFRESH_TOKEN_EXPIRATION;
-//
-//        return Jwts.builder()
-//                .setSubject(username)
-//                .setIssuedAt(new Date())
-//                .setExpiration(new Date(System.currentTimeMillis()+expiration))
-//                .signWith(Keys.hmacShaKeyFor(SECRET_KEY.getBytes()), SignatureAlgorithm.HS256)
-//                .compact();
-//    }
-//
-//    public String getUsernameFromToken(String token){
-//        return Jwts.parser().setSigningKey(SECRET_KEY.getBytes()).build().parseClaimsJws(token).getBody().getSubject();
-//    }
-//
-//    public boolean validateToken(String token){
-//        try{
-//            Jwts.parser().setSigningKey(SECRET_KEY.getBytes()).build().parseClaimsJws(token);
-//            return true;
-//        }catch(Exception e){
-//            return false;
-//        }
-//    }
-//}
 
 package com.example.ecom_backend.services.Auth;
 
@@ -84,7 +43,6 @@ public class JwtService {
             extractAllClaims(token);
             return true;
         } catch (JwtException | IllegalArgumentException e) {
-            System.out.println("Exception in validating token" + e.getMessage() + token);
             return false;
         }
     }
